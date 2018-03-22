79cce6cbcd9aaf3d40861636f6cbe0944bdc291c
askapoor <askapoor@asb10928u-c02.csil.sfu.ca>
2018-03-22 14:09:09 -0700

    4th commit

--- server_side/Facedetection2.py
+++ server_side/Facedetection2.py
@@ -24,7 +24,7 @@ arrback = np.array(myImage, dtype=np.uint8).reshape(shape)
 gray = cv.cvtColor(arrback, cv.COLOR_BGR2GRAY)
 faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 for (x,y,w,h) in faces:
-    cv.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+    cv.rectangle(arrback,(x,y),(x+w,y+h),(255,0,0),2)
     roi_gray = gray[y:y+h, x:x+w]
     roi_color = arrback[y:y+h, x:x+w]
     eyes = eye_cascade.detectMultiScale(roi_gray)
